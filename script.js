2b7221e0cb6e92a8c1f520c75130ca51d365162c
Gretszam Zabek <gz01056@srcs.loc>
2020-03-04 14:38:13 -0800

    most code

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let background = document.getElementById("waddles");
+waddles.addEventListener('mouseover', (event) => {
+    changePig(event);
+})
+waddles.addEventListener('mouseout', (event) => {
+    changePig(event);
+})
 
+function changePig(event) {
+    let pigBox = event.target;
+    let change = pigBox.style.backgroundColor;
+    if (color == "ADD8E6") {
+        pigBox.style.backgroundColor = "9ACD32";
+    } else {
+        pigBox.style.backgroundColor = "ADD8E6";
+    }
+}
