67ee21d47fdd24c159e7d8a52e9c2e8627b0886c
Gretszam Zabek <gz01056@srcs.loc>
2020-03-06 13:40:00 -0800

    ij

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let background = document.getElementById("waddles");
+var waddles = document.getElementById("waddles");
 
 waddles.addEventListener('mouseover', (event) => {
     changePig(event);
@@ -10,10 +10,10 @@ waddles.addEventListener('mouseout', (event) => {
 function changePig(event) {
     let pigBox = event.target;
     let change = pigBox.style.backgroundColor;
-    if (color == "ADD8E6") {
-        pigBox.style.backgroundColor = "9ACD32";
+    if (color == "#ADD8E6") {
+        pigBox.style.backgroundColor = "#9ACD32";
     } else {
-        pigBox.style.backgroundColor = "ADD8E6";
+        pigBox.style.backgroundColor = "#ADD8E6";
     }
 }
 
