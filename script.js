d866ef6c38e466000e442c8e968a25e018d88cf0
Phoebe Johnson <phoebejohnson@phoebes-air.srcs.loc>
2020-03-06 13:35:33 -0800

    colors

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ waddles.addEventListener('mouseout', (event) => {
 function changePig(event) {
     let pigBox = event.target;
     let change = pigBox.style.backgroundColor;
-    if (color == "ADD8E6") {
-        pigBox.style.backgroundColor = "9ACD32";
+    if (color == "#ADD8E6") {
+        pigBox.style.backgroundColor = "#9ACD32";
     } else {
-        pigBox.style.backgroundColor = "ADD8E6";
+        pigBox.style.backgroundColor = "#ADD8E6";
     }
 }
 
