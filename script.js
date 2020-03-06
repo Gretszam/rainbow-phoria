ba553122ea3bdb8fbd3b8f8e3c1fbc98dc4250c7
Gretszam Zabek <gz01056@srcs.loc>
2020-03-06 13:55:36 -0800

    ;

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ waddles.addEventListener('mouseout', (event) => {
 function changePig(event) {
     let pigBox = event.target;
     let change = pigBox.style.backgroundColor;
-    if (color == "#ADD8E6") {
+    if (change == "#ADD8E6") {
         pigBox.style.backgroundColor = "#9ACD32";
     } else {
         pigBox.style.backgroundColor = "#ADD8E6";
